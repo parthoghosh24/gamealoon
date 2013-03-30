669d778815e24e1654dea2662af0eea4c8b43922
Partho <defjam24@gmail.com>
2013-03-31 00:40:15 +0530

    All morphia instantiation and mapping pushed in DAO

--- app/utility/GloonDAO.java
+++ app/utility/GloonDAO.java
@@ -2,6 +2,11 @@ package utility;
 
 import java.net.UnknownHostException;
 
+import models.Article;
+import models.Game;
+import models.Platform;
+import models.User;
+
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.Morphia;
 import com.mongodb.Mongo;
@@ -19,18 +24,18 @@ public class GloonDAO implements GloonDataInterface{
 	static GloonDAO dataAccessLayer=null;
 	private static Mongo mongoInstance=null;
 	private Datastore gloonDatastore=null;
-	
+	private Morphia gloonMorphiaInstance=null;
 	private GloonDAO()
 	{
 	 
 		/**
 		 * Constructor logic
 		 * ->Instantiate mongo
-		 * -> create gloonDatastore
+		 * -> Instantiate morphia
 		 */
 		try {
 			mongoInstance = GloonDatabase.instantiate();		
-			
+			gloonMorphiaInstance = new Morphia();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -55,12 +60,13 @@ public class GloonDAO implements GloonDataInterface{
 	
 	/**
 	 * @param gloonMorphiaInstance is the morphia instance required for  datastore creation
-	 * 
+	 * morphia instances mapped with classes and datastore intantiated
 	 * @return gloonDatastore
 	 */
-	public Datastore initDatastore(Morphia gloonMorphiaInstance)
+	public Datastore initDatastore()
 	{		
-		gloonDatastore = gloonMorphiaInstance.createDatastore(mongoInstance, GloonDataInterface.DB_NAME);
+		gloonMorphiaInstance.map(User.class).map(Article.class).map(Game.class).map(Platform.class);
+		gloonDatastore = gloonMorphiaInstance.createDatastore(mongoInstance, GloonDataInterface.DB_NAME);		
 		return gloonDatastore;
 	}
 	
