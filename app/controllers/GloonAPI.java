d37f614cc349d75afda3a129b35a07eace1707be
Partho <defjam24@gmail.com>
2013-03-31 22:59:18 +0530

    GloonAPI created, this control will be the POC for interacting with data access layer of the app.... Will be updated time to time. Not in proper representation right now.
    Application.java is getting used for testing but it will be removed in
    future. It may so happen that app will have a single controller.

--- app/controllers/GloonAPI.java
+++ app/controllers/GloonAPI.java
@@ -1,12 +1,39 @@
 package controllers;
 
-import java.util.ArrayList;
 
+
+import java.util.ArrayList;
+import com.google.code.morphia.Datastore;
+import static play.libs.Json.toJson;
+import models.Game;
 import models.User;
 import play.mvc.Controller;
+import play.mvc.Result;
+import utility.GloonDAO;
 
 public class GloonAPI extends Controller{
 	
+	static GloonDAO daoInstance = GloonDAO.instantiateDAO();	
+	
+	public static Result getGame(String title)
+	{
+		System.out.println("title-> "+title);		
+		Datastore gloonDatastore = daoInstance.initDatastore();
+		System.out.println("gloonDatastore ->"+gloonDatastore);
+		System.out.println("gloonDatastore count-> "+gloonDatastore.getCount(Game.class));		
+		Game game = gloonDatastore.find(Game.class,"title",title).get();
+		System.out.println("Game-------> "+game);
+		
+		if(game!=null)
+		{
+			return ok(toJson(game));
+		}
+		else
+		{
+			return ok("Sorry no data found!!!");
+		}
+		
+	}
 	
 	public User getUser(String name)
 	{
