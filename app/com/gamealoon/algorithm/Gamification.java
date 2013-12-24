169cdfe0b59a995028db1fafc1d16affe35905e9
Partho <defjam24@gmail.com>
2013-12-24 17:27:47 +0530

    Level and GP calculation implemented in Gamification Models Tests updated with new test

--- app/com/gamealoon/algorithm/Gamification.java
+++ app/com/gamealoon/algorithm/Gamification.java
@@ -2,25 +2,39 @@ package com.gamealoon.algorithm;
 
 public class Gamification {
 
+	private static final int LEVEL_CONSTANT=50;
+	private static final int GP_CONSTANT=300;
 	/**
-	 * For a user, we are calculating current level via experience points
+	 * For a user, we are calculating current level via experience points. To calculate
+	 * level we are using GP of (2^n) where n is level from 0 to N. Formula to calculate level is
+	 * if xp >= 2^currLevel * 100 (level_constant), increase level by 1.
 	 * 
 	 * @param experiencePoints
 	 * @return
 	 */
-	public static Long calculateLevel(Long experiencePoints)
-	{
-		return 0L;
+	public static Long calculateLevel(long experiencePoints, long level)
+	{		
+		long levelNormalizer =(long) Math.pow(2, level); 			
+		long nextLevelScore = levelNormalizer * LEVEL_CONSTANT;
+		if(experiencePoints >= nextLevelScore)
+		{
+			++level;
+		}
+		return level;
 	}
 	
 	/**
-	 * For a user, we are calculating Gamealoon points via experience points.
-	 * 
+	 * For a user, we are calculating Gamealoon points via experience points. 
+	 * User earns 1 GP at 300 XP;
 	 * @param experiencePoints
 	 * @return
 	 */
-	public static Long calculateGP(Long experiencePoints)
+	public static Long calculateGP(long experiencePoints, long gamealoonPoints)
 	{
-		return 0L;
+		if(experiencePoints>=GP_CONSTANT)
+		{
+			++gamealoonPoints;
+		}
+		return gamealoonPoints;
 	}
 }
